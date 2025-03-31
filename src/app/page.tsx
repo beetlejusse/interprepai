2b604ad666d8da5e869b115e0ad9a4612cb8f9f9
vishalBelwal <vishalbelwal52@gmail.com>
2025-04-01 00:38:03 +0530

    chore: Optimize image loading on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,28 @@ export default function Home() {
   return (
     <main className="bg-black h-[calc(100vh-58px)]">
       <LandingPage />
-      <div className="bg-black relative bottom-16">
+      <div className="bg-black relative bottom-24">
         <BlurFade delay={0.25 * 5}>
           <NeonGradientCard
-            className="max-w-[1000px] h-fit mx-auto"
+            className="max-w-[1000px] max-h-fit mx-auto"
             borderSize={1}
             neonColors={{
               firstColor: "yellow , orange",
               secondColor: "blue, green",
             }}
           >
-            <Image
-              src={"/dashboard.png"}
-              alt="Image"
-              width={1920}
-              height={1080}
-            ></Image>
+            <div className="relative w-full h-full">
+              <Image
+                src="/dashboard.png"
+                alt="Dashboard Image"
+                width={1000}
+                height={563}
+                quality={95}
+                priority={true}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFdwI2QOQvhwAAAABJRU5ErkJggg=="
+              />
+            </div>
           </NeonGradientCard>
         </BlurFade>
       </div>
