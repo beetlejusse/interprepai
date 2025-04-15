e7e9ab8db44c2d40607153ee52f1322bdf5199ce
vishalBelwal <vishalbelwal52@gmail.com>
2025-04-16 00:53:58 +0530

    chore: Add "/dashboard" to protected routes in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isprotectedRoute = createRouteMatcher([
     '/resume-builder',
     '/api',
-    '/code-compiler'
-    
+    '/code-compiler',
+    "/dashboard"
 ])
 console.log(isprotectedRoute)
 
@@ -14,9 +14,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
         '/(api|trpc)(.*)',
     ],
 }
